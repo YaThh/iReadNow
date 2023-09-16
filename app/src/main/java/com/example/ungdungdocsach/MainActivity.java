62ab331a3d3d87a8c974ededcc64dc85b7e6555b
Thuan <giathuanvo2708@gmail.com>
2023-09-16 17:08:08 +0700

    add ui for LoginActivity

--- app/src/main/java/com/example/ungdungdocsach/MainActivity.java
+++ app/src/main/java/com/example/ungdungdocsach/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ungdungdocsach;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
 
         binding.btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(MainActivity.this, DashboardUserActivity.class));
             }
         });
     }
