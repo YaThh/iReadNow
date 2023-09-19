1568af67dff580468b492319c302cf4f0440e8ca
Thuan <giathuanvo2708@gmail.com>
2023-09-19 15:57:50 +0700

    add register function

--- app/src/main/java/com/example/ungdungdocsach/LoginActivity.java
+++ app/src/main/java/com/example/ungdungdocsach/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.ungdungdocsach;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ungdungdocsach.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLoginMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.tvNoAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ungdungdocsach/RegisterActivity.java
+++ app/src/main/java/com/example/ungdungdocsach/RegisterActivity.java
@@ -1,14 +1,170 @@
 package com.example.ungdungdocsach;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ungdungdocsach.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private ActivityRegisterBinding binding;
+
+    private FirebaseAuth auth;
+    private DatabaseReference rootRef;
+    private FirebaseFirestore db;
+
+    private Dialog registerProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Khoi tao auth
+        auth = FirebaseAuth.getInstance();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+
+        //Setup progress dialog
+        registerProgress = new Dialog(RegisterActivity.this);
+        registerProgress.setContentView(R.layout.dialog_register);
+        registerProgress.setCancelable(false);
+
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerValidate();
+            }
+        });
+    }
+
+    private String name = "", email = "", password = "", cpassword = "";
+    private void registerValidate() {
+        name = binding.edtName.getText().toString().trim();
+        email = binding.edtRegisterEmail.getText().toString().trim();
+        password = binding.edtRegisterPassword.getText().toString().trim();
+        cpassword = binding.edtConfirmPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Vui lòng nhập tên", Toast.LENGTH_SHORT).show();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(RegisterActivity.this, "Email không hợp lệ", Toast.LENGTH_SHORT).show();
+        } else if (password.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+        } else if (cpassword.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Vui lòng nhập xác nhận mật khẩu", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(cpassword)) {
+            Toast.makeText(RegisterActivity.this, "Mật khẩu và xác nhận mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
+        } else {
+            createUserAccount();
+        }
+    }
+
+    private void createUserAccount() {
+
+        registerProgress.show();
+
+        //Tao tai khoan trong firebase auth
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                updateUserInfo();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                registerProgress.dismiss();
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void updateUserInfo() {
+        registerProgress.setContentView(R.layout.dialog_saving_data);
+
+        long timestamp = System.currentTimeMillis();
+
+        //Lay uid cua user, user da dang ki nen se co uid
+        String uid = auth.getUid();
+
+        //Setup du lieu cho user
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("uid", uid);
+        hashMap.put("email", email);
+        hashMap.put("name", name);
+        hashMap.put("profileImage", "");
+        hashMap.put("userType", "user"); //User va admin
+        hashMap.put("timestamp", timestamp);
+
+
+//        //Luu du lieu vao db
+//        DatabaseReference userRef = rootRef.child("User").child(auth.getCurrentUser().getUid());
+//        userRef.setValue(hashMap)
+//        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void unused) {
+//                registerProgress.dismiss();
+//                Toast.makeText(RegisterActivity.this, "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(RegisterActivity.this, DashboardUserActivity.class));
+//                finish();
+//            }
+//        })
+//        .addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                registerProgress.dismiss();
+//                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        CollectionReference userInfoRef = db.collection("User").document(uid)
+                .collection("UserInfo");
+
+        userInfoRef.add(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        registerProgress.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, DashboardUserActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                       registerProgress.dismiss();
+                       Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
